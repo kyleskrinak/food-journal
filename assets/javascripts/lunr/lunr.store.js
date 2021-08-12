8577ed8aeabf01ff98d57f80e2f0669f14ab8c2a
kyleskrinak <kyleskrinak@users.noreply.github.com>
2021-08-12 21:15:19 +0000

    jekyll build from Action 4368e9b9b32d5b76c16bb12c15bda510f9674491

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,211 @@
+var store = [{
+        "title": "21-03-28",
+        "excerpt":"Dinner Ham salad and broccoli cheddar soup ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-03/21-03-28/"
+      },{
+        "title": "21-03-29",
+        "excerpt":"Dinner Kielbasa and cabbage From the Mrs. Skreenack;   Brown the sausage, then add the cabbage. Saute until cabbage is to your liking. Add the mustard vinaigrette dressing. ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-03/21-03-30/"
+      },{
+        "title": "21-03-30",
+        "excerpt":"Dinner Kielbasa and cabbage, added 4 ounces of summer sausage. ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-03/21-03-29/"
+      },{
+        "title": "21-03-31",
+        "excerpt":"Dinner Petite sirloin steak salad with homemade blue cheese dressing ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-03/21-03-31/"
+      },{
+        "title": "21-04-01 Holy Thursday",
+        "excerpt":"Dinner Creamy jalapeño popper chicken with cauliflower. Recipe ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-01/"
+      },{
+        "title": "21-04-02,3, Good Friday, Holy Saturday",
+        "excerpt":"Triduum Fast Water, tea (with 1 tsp HWC and one packet of Stevia) ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-02-03/"
+      },{
+        "title": "21-04-04",
+        "excerpt":"Lunch Jalapeno popper chicken Snack Homemade coconut barTurkey pepperoni Dinner Lobster with Spinach cheese puffPork Rinds with 7 oz of El Terrifico White Cheese Dip ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-04/"
+      },{
+        "title": "21-04-05",
+        "excerpt":"Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packet 20 oz diet lemonade with 2 oz ACVLunch Jalapeño Popper Chicken and 3 Buffalo Chicken thighs Schmaltz-fried chicken skinExercise Walk with Mrs. Screenack, 5 miles 10 oz water, 10 oz sugar-free lemonade, 2 oz ACV 10 oz sugar-free orange soda,...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-05/"
+      },{
+        "title": "21-04-06",
+        "excerpt":"Sleep: 72Misc Sous vide bottom round roast is running. Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packet 20 oz iced water with 2 oz ACVLunch 3 ground beef patties, 2 slices American cheese, 1 8” chaffle, Dijon mustard 20 oz diet orange sodaExercise 9 AM Dog walk, at...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-06/"
+      },{
+        "title": "21-04-07",
+        "excerpt":"Sleep: 68Misc Sous vide bottom round roast is still running. Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packet 64 oz water, splash of diet orange soda.Lunch Tuna-melt chaffle, 2 buffalo chicken thighsSnack Half-chaffle with 2 oz cream cheeseExercise Morning rosary walk with Holly, second walk at 11 AM....","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-07/"
+      },{
+        "title": "21-04-08",
+        "excerpt":"Sleep: 79Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packet 32 oz water splash of orange diet sodaLunch 12 oz bottom round roast with creamy horseradish sauce, 1 egg foo young serving with the sauce A summer sausage chaffle with 2 slices provolone cheese. Now I’m satiatedSnack 32 oz...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-08/"
+      },{
+        "title": "21-04-09",
+        "excerpt":"Misc Sore upper back for day 2. Took a naproxen and I quickly feel betterSleep: 76Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packetExercise Morning rosary walk with Holly, at 4.2k steps. Upper back is sore Afternoon walk, over 11k stepsDinner Stir-fry shrimp Coconut bar in plain yogurt with...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-09/"
+      },{
+        "title": "21-04-10",
+        "excerpt":"Misc Upper back better, still a little soreSleep: 70Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packetExercise 8 mile run. Slow pace. Lunch Leftover pot luck: stir fry shrimp, remaining bottom round roast, and 6 oz ground beefSnack Summer sausage 4 oz with mustard and some chaffles 2 oz...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-10/"
+      },{
+        "title": "21-04-11",
+        "excerpt":"Misc Sleep: 69Morning 64 oz coffee, 2 tsp HWC, 2 Stevia packetExercise 7.8 mile Sunday morning walk, my favorite exercise, around Bond Lake Hours of pollen-infested yard work 27.5kLunch Pork loin, 8 oz ground beef, and Elena’s cheesy broccoliSnack NoneDinner 3 ground beef patties with cheddar-jack cheese on a chaffle....","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-11/"
+      },{
+        "title": "21-04-12",
+        "excerpt":"Sleep: 73Exercise YMCA weight workout Neighborhood walk in the evening 10k stepsMorning 64 oz coffee, 2 tsp HWC, 2 Stevia packetLunch 3 hamburger patties on an 8” chaffle, cole slaw 4 oz summer sausage Diet ginger ale 18 oz, with ACVDinner Roasted split chicken with dill salsa verde 3.5 oz...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-12/"
+      },{
+        "title": "21-04-13",
+        "excerpt":"Sleep:   77Exercise   YMCA weight workoutMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packetLunch   other half of last night’s roasted split chicken, aldi cole slaw mixDinner   “Breakfast for Dinner”  Coconut bars with yogurt, pecans, and a packet of Stevia","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-13/"
+      },{
+        "title": "21-04-14",
+        "excerpt":"Sleep: 79Exercise YMCA weight workoutMorning 64 oz coffee, 2 tsp HWC, 2 Stevia packet Diet ginger-ale with ACVLunch Dinner for Breakfast leftovers with 8 oz ground beef added Pepperoni, pepper-jack, and cheese half-chaffle Diet ginger-ale with ACVDinner Roasted Pork Loin, cheesy cauliflower grits, arugula. 3.5 oz pork rinds with 7...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-14/"
+      },{
+        "title": "21-04-15",
+        "excerpt":"Sleep:   69Exercise   YMCA weight workoutMorning   64 oz chai tea, 2 tsp HWC, 2 Stevia packet  Diet ginger-ale with ACVLunch   Roasted pork loin with 12 oz ground beef, cheesy cauliflower and arugulaDinner ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-15/"
+      },{
+        "title": "21-04-16",
+        "excerpt":"Sleep:   74Exercise   two morning walks, 3k each  afternoon walk, over 10k steps for the day nowMorning   64 oz cold brew coffee, 2 tsp HWC, 2 Stevia packet  Diet ginger-ale with ACVLunch   Skipping, it’s Friday  Diet orange with ACVDinner   Fasted for 22 hours before dinner  Roasted salmon  Low-ish carb pizza","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-16/"
+      },{
+        "title": "21-04-17",
+        "excerpt":"Sleep:   61 Leg cramps every 45 minutes!Exercise   8 mile Saturday run, 20k step by 2 PMMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Diet orange soda with ACVLunch   low-carb pizza and salmon leftovers Snack   half 8” chaffle with 1 slice pepper jack cheese and 3 oz pepperoniDinner ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-17/"
+      },{
+        "title": "21-04-18",
+        "excerpt":"Sleep: 79 (Took one day to report?)Exercise 8 mile Sunday Bond Lake walk, 18k step by 2 PMMorning 64 oz coffee, 2 tsp HWC, 2 Stevia packets Diet orange soda with ACVLunch 12 oz ground beef with cheesy cauliflower rice and arugulaSnack Baked coconut flakes 2 oz pork rindsDinner Creamy...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-18/"
+      },{
+        "title": "21-04-19",
+        "excerpt":"Sleep:   72Exercise   Morning Y weight machine routineMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Diet orange soda with ACVLunch   12 oz ground beef, 4 eggs bakedSnack   1/4 coconut bar, 1/4 cup yogurtDinner   Monday","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-19/"
+      },{
+        "title": "21-04-20",
+        "excerpt":"Sleep: 67Exercise Morning Y weight machine routine Afternoon run/walk with KatlynMorning 64 oz coffee, 2 tsp HWC, 2 Stevia packets Diet orange soda with ACVLunch 12 oz ground beef with broccoliSnack Unsweetened baked coconut flakes, handful of sunflower seedsDinner Tuesday Chili, 3.5 oz pork rinds with El Terrifico white queso...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-20/"
+      },{
+        "title": "21-04-21",
+        "excerpt":"Sleep: 73Exercise Morning Y weight machine routine Reduced my set count from 3 to 2, increasing weight Nap, 8 AM Nap, 1 PMMorning 64 oz coffee, 2 tsp HWC, 2 Stevia packets Water with diet orange soda with ACVLunch Pot Sticker stir fry leftovers with 8 oz ground beefSnack Dinner...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-21/"
+      },{
+        "title": "21-04-22",
+        "excerpt":"Sleep:   86Exercise   Morning Y weight machine routine          Reduced my set count from 3 to 2, increasing weight      Morning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Water with diet orange soda with ACVLunch   Leftover chili with 12 oz ground beefSnack Dinner   Thursday","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-22/"
+      },{
+        "title": "21-04-23",
+        "excerpt":"Sleep:   64! What the heck!Exercise   Morning Y weight machine routineMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Water with diet orange soda with ACVLunch   OMAD, silly  lunchtime nap in hot tubDinner   HiPoké bowl from  Morrisville, NC ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-23/"
+      },{
+        "title": "21-04-24",
+        "excerpt":"Sleep: 63! Early to drive Katlyn to the airport. Killer nap at 2 - 3 PMExercise 8 mile Saturday runMorning 64 oz coffee, 2 tsp HWC, 2 Stevia packets Water with diet orange soda with ACVLunch Leftover chili over a bed of pork rinds with shredded cheddarSnack 4 oz summer...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-24/"
+      },{
+        "title": "21-04-25",
+        "excerpt":"Sleep:   66Exercise   7.5 mile Sunday morning walkMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Water with diet orange soda with ACVLunch   Leftover kale and sausage soupSnack   Pork rinds with white queso cheeseDinner ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-25/"
+      },{
+        "title": "21-04-26",
+        "excerpt":"Sleep:   79, niceExercise   Morning Y weight routineMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Water with diet orange soda with ACVLunch   Leftover kale and sausage soupSnack   Pork rinds with white queso cheeseDinner ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-26/"
+      },{
+        "title": "21-04-27",
+        "excerpt":"Sleep:   69, feh. Had the Pfizer shot yesterday, sore armExercise   Morning Y weight routineMorning   64 oz coffee, 2 tsp HWC, 2 Stevia packets  Water with diet lemonade with ACVLunch   Beef with broccoli leftovers with 6 oz ground beefSnack   Pork rinds with white queso cheeseDinner ","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/journal/21-04-27/"
+      },{
+        "title": "Jalapeño Popper Chicken Casserole",
+        "excerpt":"Time to make: 1 hr prep 1/2 - 3/4 hour stove top 20 minutes ovenIngredients 1 bunch of broccoli, cut into bite size florets 2 1/2 - 3 lbs boneless skinless chicken thighs, cut into bite size pieces 1/2 lb bacon, chopped 1 small onion, minced 2 cloves garlic minced...","categories": ["entree"],
+        "tags": ["chicken"],
+        "url": "https://kyleskrinak.github.io/food-journal/entree/2021/04/04/jalapeno-popper-chicken.html"
+      },{
+        "title": "French Onion Salisbury Steak",
+        "excerpt":"(From: Cuisine at home, April 2005, Issue 50, p. 32. Link to online version. We’ve removed the unnecessary sugar and starches from the recipe.) Makes: 4 Steaks Total Time: 45 Minutes Rating: Intermediate For the Salisbury Steaks— Combine; Divide and Form Patties: 1 1/4 lb. ground chuck 1/4 cup fresh...","categories": [],
+        "tags": ["beef"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/05/french-onion-salisbury-steak.html"
+      },{
+        "title": "Kielbasa and Cabbage Skillet",
+        "excerpt":"Mrs. Screenack uses the recipe found here. She doubles the portion of kielbasa and cabbage ingredients. ","categories": [],
+        "tags": ["sausage"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/05/kielbasa-and-cabbage-skillet.html"
+      },{
+        "title": "Spinach and Cheese Puff",
+        "excerpt":"You can find the recipe instructions at this link. Nutritional Info Servings 5.0   Amount Per Serving   calories 333   % Daily Value *   Total Fat 26 g 40 % Saturated Fat 15 g 76 % Monounsaturated Fat 4 g   Polyunsaturated Fat 1 g   Trans Fat...","categories": ["side","dish"],
+        "tags": ["dairy","eggs","spinach"],
+        "url": "https://kyleskrinak.github.io/food-journal/side/dish/2021/04/06/spinach-and-cheese-puff.html"
+      },{
+        "title": "Rachael Ray's Egg Foo Young",
+        "excerpt":"Recipe is available here. Substitutions No almonds Xanthan gum for the cornstarch Ghee for the vegetable oilNutritional Info Servings 5.0   Amount Per Serving   calories 300   % Daily Value *   Total Fat 17 g 27 % Saturated Fat 6 g 30 % Monounsaturated Fat 1 g  ...","categories": [],
+        "tags": ["eggs"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/07/rachael-ray-s-egg-foo-young.html"
+      },{
+        "title": "Stir Fry Shrimp",
+        "excerpt":"","categories": [],
+        "tags": ["shrimp","fish"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/10/stir-fry-shrimp.html"
+      },{
+        "title": "Meatzza",
+        "excerpt":"A long time family favorite. Mrs. Screenack originally found this on fastpaleo.com, but that site is no longer with us. You can see archive.org version here. She has doubled the original recipe, values shown below. Ingredients 2 lb ground beef 2 large egg 4 cloves garlic, minced 2 can chopped...","categories": [],
+        "tags": ["beef"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/11/meatzza.html"
+      },{
+        "title": "Roasted Split Chicken with Dill Salsa Verde",
+        "excerpt":"The recipe is from the Food Network’s Geoffrey Zakarian. No modifications need to make it low-carb. Nutrition Info Amount Per Serving   % Daily Value* Calories 616.7 kcal 31 % Total Fat 47.8 g 73 % Saturated Fat 9.5 g 47 % Trans Fat 0.7 g   Cholesterol 133.7 mg...","categories": [],
+        "tags": ["chicken"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/13/roasted-split-chicken-with-dill-salsa-verde.html"
+      },{
+        "title": "Breakfast for Dinner",
+        "excerpt":"This is Mrs. Screenack’s own recipe. In fact, she just made it up tonight.   Cheddar cheese  Eggs  Aldi hot breakfast sausage  Asparagus","categories": [],
+        "tags": ["eggs","sausage"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/14/breakfast-for-dinner.html"
+      },{
+        "title": "Screenack's Chaffles",
+        "excerpt":"By “Screenack’s,” I mean very simple and easy to make. I have an 8” double-sided waffle maker that’s showing the wear from tens of thousands of wheat waffles I’ve made in the past. This makes 5 8” chaffles. Ingredients 8 eggs 8 oz Parmesan cheese wedge 1.75 oz unprocessed pork...","categories": [],
+        "tags": [],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/14/kyle-s-chaffles.html"
+      },{
+        "title": "Roasted Pork Loin with Ranch rub",
+        "excerpt":"This is a pork loin with a homemade Ranch dressing mix rub before going in the oven French Dressing Mix   5 T dried minced onions  7 t dried parsley flakes  4 t salt  1 t garlic powder  1 t dried dill","categories": [],
+        "tags": ["pork"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/15/roasted-pork-loin-with-ranch-rub.html"
+      },{
+        "title": "Low Carb White Spinach Pizza",
+        "excerpt":"Crust recipe You can find the crust recipe here. Salmon included in the picture because — hey! Salmon! Topping (not from the above recipe) 2 T olive oil and 3 minced glaric cloves brushed onto crushMix together and spread on crust: 1/3 c ricotta 1/8 t dried oregano 1/8 t...","categories": [],
+        "tags": ["side"],
+        "url": "https://kyleskrinak.github.io/food-journal/2021/04/17/low-carb-white-spinach-pizza.html"
+      }]
